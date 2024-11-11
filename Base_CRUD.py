bc7d2d8a78515d1d3cf7a68cd35a8a91670c3b29
MarinaMM-IDB-23-11 <manmarmich@gmail.com>
2024-11-11 22:05:30 +0300

    Protection and other things

--- Base_CRUD.py
+++ Base_CRUD.py
@@ -4,10 +4,10 @@ from typing import Union
 
 class AudioFile:
     def __init__(self, name_file: str, author: str, duration: int, file: str):
-        self.name_file = name_file
-        self.author = author
-        self.duration = duration #the duration of the file playback in minutes
-        self.file = file #audio simulation
+        self.name_file: str = name_file
+        self.author: str = author
+        self.duration: int = duration #the duration of the file playback in minutes
+        self.file: str = file #audio simulation
 
     def to_dict(self) -> dict[str, any]: #converting an object to JSON format
         return {
@@ -19,7 +19,7 @@ class AudioFile:
         }
 
     def to_xml(self) -> ET.Element: #translation to XML format
-        audio_elem = ET.Element("AudioFile")
+        audio_elem: ET.Element = ET.Element("AudioFile")
         ET.SubElement(audio_elem, "name_file").text = self.name_file
         ET.SubElement(audio_elem, "author").text = self.author if self.author else ""
         ET.SubElement(audio_elem, "duration").text = str(self.duration)
@@ -38,9 +38,10 @@ class AudioEditor: #CRUD
             if af.name_file == name_file: #Another class is responsible for the "playback" of the file
                 print (f"Name: {af.name_file}. Author: {af.author}. Duration: {af.duration} minutes")
                 break
+        print("No such audiofile.")
 
     def read_all_audio_files(self) -> None: #read all files
-        if not self.audio_files:  # Проверка на наличие аудиофайлов
+        if not self.audio_files:  # Checking for audio files
             print("No audio files available.")
             return
         for af in self.audio_files:
@@ -48,20 +49,22 @@ class AudioEditor: #CRUD
 
     def update_audio_file(self, name_file: str) -> None: #update
         for af in self.audio_files:
-            if af._name_file == name_file:
-                af._name_file = input("Enter the name of the audio: ")
-                af._author = input("Enter the author`s name: ")
+            if af.name_file == name_file:
+                af.name_file = input("Enter the name of the audio: ")
+                af.author = input("Enter the author`s name: ")
+        print("No such audiofile.")
 
     def delete_audio_file(self, name_file: str) -> None: #delete
         for af in self.audio_files:
             if af.name_file == name_file:
                 self.audio_files.remove(af)
                 break
+        print("No such audiofile.")
 
 class Effects(AudioFile): #here you can apply an audio effect to a file
     def __init__(self, name_file: str, author: str, duration: int, file: str, effect: str):
         super().__init__(name_file, author, duration, file)
-        self.effect = effect
+        self.effect: str = effect
 
     def to_dict(self) -> dict [str, any]: #converting an object to JSON format
         return {
@@ -74,6 +77,6 @@ class Effects(AudioFile): #here you can apply an audio effect to a file
         }
 
     def to_xml(self) -> ET.Element: #translation to XML format
-        effect_elem = super().to_xml()
+        effect_elem: ET.Element = super().to_xml()
         ET.SubElement(effect_elem, "effect").text = self.effect
         return effect_elem
--- Functional.py
+++ Functional.py
@@ -3,14 +3,15 @@ from JSON_and_XML import JSONSerializer, XMLSerializer
 
 
 class Playlist:  #Tracks are played here
-    def __init__(self, editor_zero):
-        self.editor = editor_zero
+    def __init__(self, editor_zero: AudioEditor):
+        self.editor: AudioEditor = editor_zero
 
     def play_audiofile(self, name_file: str) -> None:  #plays the selected audio file by name
         for af in self.editor.audio_files:
             if af.name_file == name_file:
                 print(af.file)
                 break
+        print("No such audiofile.")
 
     def play_all(self) -> None:
         for af in self.editor.audio_files:
@@ -18,8 +19,8 @@ class Playlist:  #Tracks are played here
 
 
 class Cropping:  #this is where the audio file is cropped
-    def __init__(self, editor_zero):
-        self.editor = editor_zero
+    def __init__(self, editor_zero: AudioEditor):
+        self.editor: AudioEditor = editor_zero
 
     def crop(self, name_file: str) -> str:
         for af in self.editor.audio_files:
@@ -29,8 +30,8 @@ class Cropping:  #this is where the audio file is cropped
 
 
 class Continuation:  #here you can "continue" the audio
-    def __init__(self, editor_zero):
-        self.editor = editor_zero
+    def __init__(self, editor_zero: AudioEditor):
+        self.editor: AudioEditor = editor_zero
 
     def proceed(self, name_file: str) -> str:
         for af in self.editor.audio_files:
@@ -38,11 +39,12 @@ class Continuation:  #here you can "continue" the audio
                 verse: str = af.file.split("-", 1)[0]
                 result: str = af.file + "-" + verse
                 return result  #before "Uc-Uc", after "Uc-Uc-Uc"
+        print("No such audiofile.")
 
 
 class Mix:  #You can mix audio files here
-    def __init__(self, editor_zero):
-        self.editor = editor_zero
+    def __init__(self, editor_zero: AudioEditor):
+        self.editor: AudioEditor = editor_zero
 
     def connect(self, name_file1: str, name_file2: str) -> str:  #just by combining them
         for af1 in self.editor.audio_files:
@@ -56,22 +58,25 @@ class Mix:  #You can mix audio files here
         for af1 in self.editor.audio_files:
             for af2 in self.editor.audio_files:
                 if af1.name_file == name_file1 and af2.name_file == name_file2:
-                    verse1: str = af1.file.split("-", 1)[0]
-                    verse2: str = af2.file.split("-", 1)[0]
-                    result: str = verse1 + "-" + verse2
-                    while count != af1.duration - 1:
-                        result += "-" + result
-                        count += 1
-                    return result  #before "La-La" and "Uc-Uc", after "La-Uc-La-Uc"
+                    if af1.duration == af2.duration:
+                        verse1: str = af1.file.split("-", 1)[0]
+                        verse2: str = af2.file.split("-", 1)[0]
+                        result: str = verse1 + "-" + verse2
+                        while count != af1.duration - 1:
+                            result += "-" + result
+                            count += 1
+                        return result  #before "La-La" and "Uc-Uc", after "La-Uc-La-Uc"
+                    else:
+                        return ""
 
 
 class UserInterface:
-    def __init__(self, editor_zero):
-        self.editor = editor_zero
-        self.playlist = Playlist(self.editor)
-        self.cropping = Cropping(self.editor)
-        self.continuation = Continuation(self.editor)
-        self.mix = Mix(self.editor)
+    def __init__(self, editor_zero: AudioEditor):
+        self.editor: AudioEditor = editor_zero
+        self.playlist: Playlist = Playlist(self.editor)
+        self.cropping: Cropping = Cropping(self.editor)
+        self.continuation: Continuation = Continuation(self.editor)
+        self.mix: Mix = Mix(self.editor)
 
     @staticmethod
     def show_menu() -> None:
@@ -83,7 +88,8 @@ class UserInterface:
         print("6. Continue an audio file ")
         print("7. Mix audio files")
         print("8. Add an effect")
-        print("9. Exit")
+        print("9. Update the audio file")
+        print("10. Exit")
 
     @staticmethod
     def counting_the_duration(file: str) -> int:
@@ -91,21 +97,24 @@ class UserInterface:
         return duration
 
     def upload_audiofiles(self) -> None:
-        print("Upload files from JSON(press 1) or XML (press 2)?")
-        choice: str  = input()
-        if choice == "1":
-            jsoneditor = JSONSerializer(self.editor)
-            self.editor = jsoneditor.load_from_json("AudiofilesJSON.json")
-        elif choice == "2":
-            xml_editor = XMLSerializer(self.editor)
-            self.editor = xml_editor.load_from_xml("AudiofilesXML.xml")
-        #self.editor.read_all_audio_files()
+        while True:
+            choice: str = input("Upload files from JSON(press 1) or XML (press 2)? ")
+            if choice == "1":
+                jsoneditor: JSONSerializer = JSONSerializer(self.editor)
+                self.editor: AudioEditor = jsoneditor.load_from_json("AudiofilesJSON.json")
+                break
+            elif choice == "2":
+                xml_editor: XMLSerializer = XMLSerializer(self.editor)
+                self.editor: AudioEditor = xml_editor.load_from_xml("AudiofilesXML.xml")
+                break
+            else:
+                print("Invalid number, try again.")
 
     def save_to_files(self) -> None:
-        json_serializer = JSONSerializer(self.editor)
+        json_serializer: JSONSerializer = JSONSerializer(self.editor)
         json_serializer.save_to_json("AudiofilesJSON.json")
 
-        xml_serializer = XMLSerializer(self.editor)
+        xml_serializer: XMLSerializer = XMLSerializer(self.editor)
         xml_serializer.save_to_xml("AudiofilesXML.xml")
 
     def run(self):
@@ -126,81 +135,101 @@ class UserInterface:
                 self.editor.create_audio_file(audiofile)
 
             elif choice == "2":
-                name_file = input("Enter the name of the file you want to delete: ")
+                name_file: str = input("Enter the name of the file you want to delete: ")
                 self.editor.delete_audio_file(name_file)
 
             elif choice == "3":
-                name_file = input("Enter the name of the file you want to list: ")
+                name_file: str = input("Enter the name of the file you want to list: ")
                 self.playlist.play_audiofile(name_file)
 
             elif choice == "4":
                 self.playlist.play_all()
 
             elif choice == "5":
-                name_file = input("Enter the name of the file you want to crop: ")
-                cropped_data = self.cropping.crop(name_file)
-                if cropped_data:  # Проверяем, что обрезка прошла успешно
-                    # Создаем новый аудиофайл с обрезанными данными
+                name_file: str = input("Enter the name of the file you want to crop: ")
+                cropped_data: str = self.cropping.crop(name_file)
+                if cropped_data:  # We check that the pruning was successful
+                    # Creating a new audio file with cropped data
                     for af in self.editor.audio_files:
                         if af.name_file == name_file:
-                            new_duration = self.counting_the_duration(cropped_data)  # Пересчитываем новую продолжительность
-                            cropped_audio_file = AudioFile(af.name_file, af.author, new_duration, cropped_data)
+                            new_duration: int = self.counting_the_duration(
+                                cropped_data)  # Recalculating the new duration
+                            cropped_audio_file: AudioFile = AudioFile(af.name_file, af.author, new_duration,
+                                                                      cropped_data)
                             self.editor.delete_audio_file(af.name_file)
-                            self.editor.create_audio_file(cropped_audio_file)  # Добавляем новый файл в self.editor
+                            self.editor.create_audio_file(cropped_audio_file)  # Adding a new file to self.editor
                             break
                 else:
-                    pass #print("Cropping failed or audio file not found.")
+                    print("Cropping failed or audio file not found.")
 
             elif choice == "6":
-                name_file = input("Enter the name of the file you want to continue: ")
-                continuation_data = self.continuation.proceed(name_file)
-                if continuation_data:  # Проверяем, что обрезка прошла успешно
-                    # Создаем новый аудиофайл с обрезанными данными
+                name_file: str = input("Enter the name of the file you want to continue: ")
+                continuation_data: str = self.continuation.proceed(name_file)
+                if continuation_data:
                     for af in self.editor.audio_files:
                         if af.name_file == name_file:
-                            new_duration = self.counting_the_duration(continuation_data)  # Пересчитываем новую продолжительность
-                            continuation_audio_file = AudioFile(af.name_file, af.author, new_duration, continuation_data)
+                            new_duration: int = self.counting_the_duration(continuation_data)
+                            continuation_audio_file: AudioFile = AudioFile(af.name_file, af.author, new_duration,
+                                                                           continuation_data)
                             self.editor.delete_audio_file(af.name_file)
-                            self.editor.create_audio_file(continuation_audio_file)  # Добавляем новый файл в self.editor
+                            self.editor.create_audio_file(continuation_audio_file)
                             break
                 else:
-                    pass #print("Cropping failed or audio file not found.")
+                    print("Continuation failed or audio file not found.")
 
             elif choice == "7":
-                name_file1 = input("Enter the name of the file 1: ")
-                name_file2 = input("Enter the name of the file 2: ")
-                choice_mix: str = input(
-                    "If you want to connect two audio files, enter 1. If you want to mix them, enter 2. ")
-                if choice_mix == "1":
-                    connect_data = self.mix.connect(name_file1, name_file2)
-                    new_duration = self.counting_the_duration(connect_data)
-                    name_file: str = input("Enter the name of the audio file: ")
-                    author: str = input("Enter the author of the audio file: ")
-                    connect_audio_file = AudioFile(name_file, author, new_duration, connect_data)
-                    self.editor.create_audio_file(connect_audio_file)
-                elif choice_mix == "2":
-                    remix_data: str = self.mix.remix(name_file1, name_file2)
-                    new_duration = self.counting_the_duration(remix_data)
-                    name_file: str = input("Enter the name of the audio file: ")
-                    author: str = input("Enter the author of the audio file: ")
-                    remix_audio_file = AudioFile(name_file, author, new_duration, remix_data)
-                    self.editor.create_audio_file(remix_audio_file)
-                else:
-                    pass
+                name_file1: str = input("Enter the name of the file 1: ")
+                name_file2: str = input("Enter the name of the file 2: ")
+
+                while True:
+                    choice_mix: str = input(
+                        "If you want to connect two audio files, enter 1. If you want to mix them, enter 2 "
+                        "(WARNING: The remix function only works with files of the same duration). ")
+                    if choice_mix == "1":
+                        connect_data: str = self.mix.connect(name_file1, name_file2)
+                        if connect_data:
+                            new_duration: int = self.counting_the_duration(connect_data)
+                            name_file: str = input("Enter the name of the audio file: ")
+                            author: str = input("Enter the author of the audio file: ")
+                            connect_audio_file: AudioFile = AudioFile(name_file, author, new_duration, connect_data)
+                            self.editor.create_audio_file(connect_audio_file)
+                        else:
+                            print("Connect failed or audio file not found.")
+                        break
+                    elif choice_mix == "2":
+                        remix_data: str = self.mix.remix(name_file1, name_file2)
+                        if remix_data:
+                            new_duration: int = self.counting_the_duration(remix_data)
+                            name_file: str = input("Enter the name of the audio file: ")
+                            author: str = input("Enter the author of the audio file: ")
+                            remix_audio_file: AudioFile = AudioFile(name_file, author, new_duration, remix_data)
+                            self.editor.create_audio_file(remix_audio_file)
+                        else:
+                            print("Remix failed or audio file not found.")
+                        break
+                    else:
+                        print("Invalid number, try again.")
 
             elif choice == "8":
-                name_file = input("Write the name of the file to which you want to add the effect: ")
+                name_file: str = input("Write the name of the file to which you want to add the effect: ")
                 effect: str = input("Write the name of the effect to which you want to add: ")
                 for af in self.editor.audio_files:
                     if isinstance(af, AudioFile) and af.name_file == name_file:
-                        effect_audiofile = Effects(af.name_file, af.author, af.duration, af.file, effect)
+                        effect_audiofile: Effects = Effects(af.name_file, af.author, af.duration, af.file, effect)
                         self.editor.delete_audio_file(name_file)
                         self.editor.create_audio_file(effect_audiofile)
+                        break
+                print("Sorry, there may not be a suitable file or file with that name.")
 
             elif choice == "9":
+                name_file: str = input("Enter the name of the file you want to update: ")
+                self.editor.update_audio_file(name_file)
+
+            elif choice == "10":
                 break
+
             else:
-                pass
+                print("Invalid number, try again.")
             self.save_to_files()
 
 
